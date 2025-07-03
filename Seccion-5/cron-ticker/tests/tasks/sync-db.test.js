37621e64a2dbe034afde872a7ed2abd59eda5a0b
Eduardo Salas Gonzalez <89312228+EduardoSalasG@users.noreply.github.com>
2025-07-03 17:15:16 -0400

    Fin seccion 5

---
+++ Seccion-5/cron-ticker/tests/tasks/sync-db.test.js
@@ -0,0 +1,15 @@
+const { syncDB } = require('../../tasks/sync-db')
+
+describe('Pruebas en Sync-DB', () => {
+
+    test('Debe de ejecutar el proceso 2 veces ', () => {
+
+        syncDB();
+        const times = syncDB();
+        console.log('Se llamó ', times)
+
+        expect(times).toBe(2)
+
+    })
+
+})  
